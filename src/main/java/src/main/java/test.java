e8bd2fb4d21d94edd6267bd1ad4766c98a8ce54d
zhangdabao2020 <dabao@cug.edu.cn>
2021-10-12 10:08:43 +0800

    删除多余注释

--- src/main/java/src/main/java/test.java
+++ src/main/java/src/main/java/test.java
@@ -26,13 +26,7 @@ public class test {
         arrtibureVisite atrVisit = new arrtibureVisite();
         Double result =   atrVisit.visit(tree);
         System.out.println("二进制的值为："+result);
-       //  ParseTree tree1 = CBpar.binaryNum();
 
-       // RuleContext treer = CBpar.binaryNum();
-       // System.out.println(treer.toString(CBpar));
-
-       // System.out.println(tree.);
-       // System.out.println(tree.toString(CBpar));
 
         //利用属性文法 计算 实数的值
           String real = "13.6875";
@@ -47,40 +41,13 @@ public class test {
         System.out.println("实数字符串的值是："+RealNumVal);
 
 
-     /*    String sql = "Select 'abc' as a, `hahah` as c  From a aS table;";
-        CharStream input = CharStreams.fromString(sql);
 
-         CustomSqlLexer lex = new CustomSqlLexer(input); //词法分析
-
-         CommonTokenStream tokens = new CommonTokenStream(lex); //token 流
-
-         CustomSqlParser par = new CustomSqlParser(tokens);  //语法分析
-         CustomSqlParser.StaContext tree = par.sta(); // 获取某一个语法书，对应 表达式中的规则。
-         System.out.println(tree.toStringTree(par)); //打印规则
-
-         ParseTreeWalker walker = new ParseTreeWalker();*/
-
-        // CustomSqlListener comsql = new CustomSqlBaseListener(par);
-         // ParseTree tree = par.
-         // CustomSqlParser.ExprContext tree = par.expr(); //表达式
-         //CustomSqlVisitor tv = new CustomSqlVisitor();
-         //  tv.visit(tree);
 
 
 
     }
 
-    /*    static class CustomSqlVisitor extends CustomSqlBaseVisitor<Void> {
-            @Override
-            public Void visitAdd(CustomSqlParser.AddContext ctx) {
 
-             *//*   System.out.println("========= test add");
-                System.out.println("first arg: " + ctx.expr(0).getText());
-                System.out.println("second arg: " + ctx.expr(1).getText());*//*
-
-                return super.visitAdd(ctx);
-            }
-        }*/
 
 
 }
